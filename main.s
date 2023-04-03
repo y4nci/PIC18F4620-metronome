53de3a86ff532320ff934062b21c8ed02b47f721
y4nci <baran.yanci@metu.edu.tr>
2023-04-03 15:57:43 +0300

    initial commit

---
+++ main.s
@@ -0,0 +1,42 @@
+PROCESSOR    18F4620
+
+#include <xc.inc>
+
+; CONFIGURATION (DO NOT EDIT)
+CONFIG OSC = HSPLL      ; Oscillator Selection bits (HS oscillator, PLL enabled (Clock Frequency = 4 x FOSC1))
+CONFIG FCMEN = OFF      ; Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+CONFIG IESO = OFF       ; Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+; CONFIG2L
+CONFIG PWRT = ON        ; Power-up Timer Enable bit (PWRT enabled)
+CONFIG BOREN = OFF      ; Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+CONFIG BORV = 3         ; Brown Out Reset Voltage bits (Minimum setting)
+; CONFIG2H
+CONFIG WDT = OFF        ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+; CONFIG3H
+CONFIG PBADEN = OFF     ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+CONFIG LPT1OSC = OFF    ; Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+CONFIG MCLRE = ON       ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+; CONFIG4L
+CONFIG LVP = OFF        ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+CONFIG XINST = OFF      ; Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+; GLOBAL SYMBOLS
+; You need to add your variables here if you want to debug them.
+GLOBAL var1
+
+; Define space for the variables in RAM
+PSECT udata_acs
+var1:
+    DS 1 
+
+PSECT resetVec,class=CODE,reloc=2
+resetVec:
+    goto       main
+
+PSECT CODE
+main:
+  clrf var1 ; var1 = 0
+main_loop:
+  goto main_loop
+
+end resetVec
